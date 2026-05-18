0d7aad37fb1d6e82c5a6ff5f421db72a73cb97d3
Kelvin Smith <oneKelvinSmith@gmail.com>
2026-05-18 14:22:28 +0100

    `remove_element` second pass
    Maybe more memory efficient

--- packages/leetcode/src/leetcode/remove_element.py
+++ packages/leetcode/src/leetcode/remove_element.py
@@ -6,13 +6,13 @@ https://leetcode.com/problems/remove-element/
 
 class Solution:
     def remove_element(self, nums: list[int], val: int) -> int:
-        indices: list[int] = []
-        for i, num in enumerate(nums):
-            if num == val:
-                indices.append(i)
+        shifts: int = 0
 
-        for ii, i in enumerate(indices):
-            relative_index = i - ii
-            nums[relative_index:] = nums[relative_index + 1 :]
+        for i in range(len(nums)):
+            relative_index = i - shifts
+            num = nums[relative_index]
+            if num == val:
+                nums[relative_index:] = nums[relative_index + 1 :]
+                shifts += 1
 
         return len(nums)
