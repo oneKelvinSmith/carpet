c7392b483f6af446df242d78b41acfa50a57da63
Kelvin Smith <oneKelvinSmith@gmail.com>
2026-05-18 13:45:51 +0100

    `remove_element` first pass

--- packages/leetcode/src/leetcode/remove_element.py
+++ packages/leetcode/src/leetcode/remove_element.py
@@ -6,4 +6,13 @@ https://leetcode.com/problems/remove-element/
 
 class Solution:
     def remove_element(self, nums: list[int], val: int) -> int:
-        return nums[val]
+        indices: list[int] = []
+        for i, num in enumerate(nums):
+            if num == val:
+                indices.append(i)
+
+        for ii, i in enumerate(indices):
+            relative_index = i - ii
+            nums[relative_index:] = nums[relative_index + 1 :]
+
+        return len(nums)
