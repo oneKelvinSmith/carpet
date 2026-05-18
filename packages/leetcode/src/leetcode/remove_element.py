aa7de36eb0a89f7ed9d22ba95d48e87835df8453
Kelvin Smith <oneKelvinSmith@gmail.com>
2026-05-18 14:31:49 +0100

    `remove_element` keep a list of matches and re-assign
    This must be faster the changing the list on every match.

--- packages/leetcode/src/leetcode/remove_element.py
+++ packages/leetcode/src/leetcode/remove_element.py
@@ -6,13 +6,12 @@ https://leetcode.com/problems/remove-element/
 
 class Solution:
     def remove_element(self, nums: list[int], val: int) -> int:
-        shifts: int = 0
+        matches: list[int] = []
 
-        for i in range(len(nums)):
-            relative_index = i - shifts
-            num = nums[relative_index]
-            if num == val:
-                nums.pop(relative_index)
-                shifts += 1
+        for num in nums:
+            if num != val:
+                matches.append(num)
+
+        nums = matches
 
         return len(nums)
